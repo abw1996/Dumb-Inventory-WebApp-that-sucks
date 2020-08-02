9d18b580aad0ccb7a7f3e2e90d155351913fb3c8
Ashton Williams <49693678+abw1996@users.noreply.github.com>
2020-08-01 21:18:42 -0400

    Add files via upload

---
+++ Pages/Iventory/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using InventoryEditorPage.Data;
+using InventoryEditorPage.Models;
+
+namespace InventoryEditorPage.Pages.Iventory
+{
+    public class IndexModel : PageModel
+    {
+        private readonly InventoryEditorPage.Data.InventoryEditorPageContext _context;
+
+        public IndexModel(InventoryEditorPage.Data.InventoryEditorPageContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Inventory> Inventory { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Inventory = await _context.Inventory.ToListAsync();
+        }
+    }
+}
